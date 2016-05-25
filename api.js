a6cb26421f70946a9e9a2ee16df4dade502e81a5
Someone <someone@example.com>
2016-05-25 16:09:13 -0700

    Added docs

---
+++ api.js
@@ -0,0 +1,29 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "$http",
+        "$q",
+        "$routing",
+        "Deferred",
+        "Mimeo",
+        "Module",
+        "Promise"
+    ],
+    "modules": [
+        "Builtins",
+        "Mimeo"
+    ],
+    "allModules": [
+        {
+            "displayName": "Builtins",
+            "name": "Builtins"
+        },
+        {
+            "displayName": "Mimeo",
+            "name": "Mimeo",
+            "description": "The mimeo modules describes the use of the mimeo framework."
+        }
+    ],
+    "elements": []
+} };
+});
